861c7c281edb1faab3d5a21d5bdf18094d1e5f91
ajeet001 <ajeetdotdev@gmail.com>
2020-07-29 13:12:11 +0530

    Added chapter_1.py file

---
+++ chapter_1.py
@@ -0,0 +1,37 @@
+"""
+
+Chapter 1
+Name: Ajeet
+Collaborators: None
+Time spent:
+
+---------------------------------------
+"""
+
+""" 1. Write an English sentence with understandable semantics but incorrect syntax.
+Write another English sentence which has correct syntax but has semantic errors.
+
+Solution:
+If there is a single syntax error anywhere in your program, Python will display an error message and quit,
+and you will not be able to run your program. For example, in English, a sentence must begin with a capital letter and end with a period.
+this sentence contains a syntax error. So does this one
+
+If there is a semantic error in your program, it will run successfully, in the sense that the computer will not generate any error messages, but it will produce the output you wanted to see. Specifically, it will do what you told it to do by writing the program.  The meaning of the program (its semantics) is wrong
+
+"""
+
+
+
+"""
+Concepts from Book:
+
+semantic error
+    An error in a program that makes it do something other than what the programmer intended.
+semantics
+    The meaning of a program.
+syntax
+    The structure of a program.
+syntax error
+    An error in a program that makes it impossible to parse — and therefore impossible to interpret.
+
+"""
